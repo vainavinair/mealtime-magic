8a779bb864998ee562f0e472363d39befa7726ff
VanV <vainavinair2009558@gmail.com>
2023-02-14 23:16:59 +0530

    fixed bug

--- lib/show_recipe.dart
+++ lib/show_recipe.dart
@@ -29,9 +29,11 @@ class _ShowRecipeState extends State<ShowRecipe> {
     super.initState();
     _loadRecipes();
     _timer = Timer(Duration(seconds: 10), () {
-      setState(() {
-        isLoading = false;
-      });
+      if (recipeList.isEmpty) {
+        setState(() {
+          isLoading = false;
+        });
+      }
     });
   }
 
@@ -62,126 +64,105 @@ class _ShowRecipeState extends State<ShowRecipe> {
           // SEARCH BAR
           title: SearchBar(),
         ),
-        body: isLoading == false
-            ? Center(
-                child: Container(
-                child: Column(
-                  children: [
-                    Text(
-                      "NO RECIPIES FOUND",
-                      style:
-                          TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+        body: Container(
+          color: Theme.of(context).primaryColor,
+          child: ListView.builder(
+              itemCount: recipeList.length,
+              shrinkWrap: true,
+              itemBuilder: (context, index) {
+                return InkWell(
+                  onTap: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) =>
+                                RecipeView(recipeList[index].url)));
+                  },
+                  child: Card(
+                    elevation: 10.0,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(16.0),
                     ),
-                    Image.asset('assets/images/notfound.png')
-                  ],
-                ),
-              ))
-            : recipeList.isEmpty
-                ? Center(child: CircularProgressIndicator())
-                : Container(
-                    color: Theme.of(context).primaryColor,
-                    child: ListView.builder(
-                        itemCount: recipeList.length,
-                        shrinkWrap: true,
-                        itemBuilder: (context, index) {
-                          return InkWell(
-                            onTap: () {
-                              Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                      builder: (context) =>
-                                          RecipeView(recipeList[index].url)));
-                            },
-                            child: Card(
-                              elevation: 10.0,
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(16.0),
-                              ),
-                              margin: EdgeInsets.all(10.0),
-                              child: Stack(
+                    margin: EdgeInsets.all(10.0),
+                    child: Stack(
+                      children: [
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(16.0),
+                          child: Image.network(
+                            recipeList[index].image,
+                            fit: BoxFit.cover,
+                            height: 200,
+                            errorBuilder: (context, error, stackTrace) =>
+                                Container(),
+                            width: MediaQuery.of(context).size.width,
+                          ),
+                        ),
+                        Positioned(
+                          right: 0,
+                          left: 0,
+                          child: Container(
+                            height: 20,
+                            width: 100,
+                            padding: EdgeInsets.all(2),
+                            decoration: BoxDecoration(
+                                color: Color(0xffFFEDE9),
+                                borderRadius: BorderRadius.only(
+                                    topLeft: Radius.circular(16),
+                                    topRight: Radius.circular(16),
+                                    bottomRight: Radius.circular(5),
+                                    bottomLeft: Radius.circular(5))),
+                            child: Center(
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
                                 children: [
-                                  ClipRRect(
-                                    borderRadius: BorderRadius.circular(16.0),
-                                    child: Image.network(
-                                      recipeList[index].image,
-                                      fit: BoxFit.cover,
-                                      height: 200,
-                                      errorBuilder:
-                                          (context, error, stackTrace) =>
-                                              Container(),
-                                      width: MediaQuery.of(context).size.width,
-                                    ),
+                                  Icon(
+                                    Icons.language_outlined,
+                                    size: 18,
+                                    color: Color(0xffB07568),
                                   ),
-                                  Positioned(
-                                    right: 0,
-                                    left: 0,
-                                    child: Container(
-                                      height: 20,
-                                      width: 100,
-                                      padding: EdgeInsets.all(2),
-                                      decoration: BoxDecoration(
-                                          color: Color(0xffFFEDE9),
-                                          borderRadius: BorderRadius.only(
-                                              topLeft: Radius.circular(16),
-                                              topRight: Radius.circular(16),
-                                              bottomRight: Radius.circular(5),
-                                              bottomLeft: Radius.circular(5))),
-                                      child: Center(
-                                        child: Row(
-                                          mainAxisAlignment:
-                                              MainAxisAlignment.center,
-                                          children: [
-                                            Icon(
-                                              Icons.language_outlined,
-                                              size: 18,
-                                              color: Color(0xffB07568),
-                                            ),
-                                            Text(
-                                              recipeList[index]
-                                                  .cuisineType
-                                                  .toUpperCase(),
-                                              style: TextStyle(
-                                                  color: Color(0xffB07568),
-                                                  fontSize: 14,
-                                                  fontWeight: FontWeight.w600),
-                                            ),
-                                          ],
-                                        ),
-                                      ),
-                                    ),
-                                  ),
-                                  Positioned(
-                                    right: 0,
-                                    left: 0,
-                                    bottom: 0,
-                                    child: Container(
-                                      height: 20,
-                                      width: 100,
-                                      padding: EdgeInsets.all(2),
-                                      decoration: BoxDecoration(
-                                          color: Color(0xffFFEDE9),
-                                          borderRadius: BorderRadius.only(
-                                              topLeft: Radius.circular(5),
-                                              topRight: Radius.circular(5),
-                                              bottomLeft: Radius.circular(16),
-                                              bottomRight:
-                                                  Radius.circular(16))),
-                                      child: Center(
-                                        child: Text(
-                                          recipeList[index].label.toString(),
-                                          style: TextStyle(
-                                              color: Color(0xffB07568),
-                                              fontSize: 16,
-                                              fontWeight: FontWeight.w700),
-                                        ),
-                                      ),
-                                    ),
+                                  Text(
+                                    recipeList[index].cuisineType.toUpperCase(),
+                                    style: TextStyle(
+                                        color: Color(0xffB07568),
+                                        fontSize: 14,
+                                        fontWeight: FontWeight.w600),
                                   ),
                                 ],
                               ),
                             ),
-                          );
-                        }),
-                  ));
+                          ),
+                        ),
+                        Positioned(
+                          right: 0,
+                          left: 0,
+                          bottom: 0,
+                          child: Container(
+                            height: 20,
+                            width: 100,
+                            padding: EdgeInsets.all(2),
+                            decoration: BoxDecoration(
+                                color: Color(0xffFFEDE9),
+                                borderRadius: BorderRadius.only(
+                                    topLeft: Radius.circular(5),
+                                    topRight: Radius.circular(5),
+                                    bottomLeft: Radius.circular(16),
+                                    bottomRight: Radius.circular(16))),
+                            child: Center(
+                              child: Text(
+                                recipeList[index].label.toString(),
+                                style: TextStyle(
+                                    color: Color(0xffB07568),
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w700),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              }),
+        ));
   }
 }
